17620eb286e5714c7e584d822ef639ff6254dc96
taye000 <taylorgitari@gmail.com>
2023-02-15 20:27:21 +0300

    update photo route

--- server/src/controllers/updateProfile.ts
+++ server/src/controllers/updateProfile.ts
@@ -1,6 +1,33 @@
 import { Request, Response } from "express";
 import User from "../models/users";
 import validator from "validator";
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { cloudinary } from "../config";
+
+//init cloudinary storage
+const cloudinaryStorage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+});
+//init multer with cloudinary storage
+const upload = multer({ storage: cloudinaryStorage });
+
+//update profile photo controller
+export const updateProfilePhoto = async (req: Request, res: Response) => {
+  const user = await User.findById(req?.userId);
+  if (!user) {
+    res.send({ msg: "Unauthorized access" });
+  } else {
+    const photo = req?.file?.path;
+    try {
+      await User.findByIdAndUpdate(req?.userId, {
+        photo,
+      });
+    } catch (error) {
+      res.send({ msg: "Error uploading photo" });
+    }
+  }
+};
 
 export const updateProfile = async (req: Request, res: Response) => {
   const { name, email, photo } = req.body;
